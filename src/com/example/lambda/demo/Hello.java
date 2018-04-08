8893820ce6325c6f6a45248e3d091042f8cc4d29
amitadmin <amitnanda0218@gmail.com>
2018-04-08 23:14:02 +0100

    branching version changes again

--- src/com/example/lambda/demo/Hello.java
+++ src/com/example/lambda/demo/Hello.java
@@ -15,7 +15,9 @@ public class Hello implements RequestHandler<Object, String> {
         System.out.println("This is branching version 3");
         System.out.println("This is branching version 4");
         System.out.println("This is branching version 5");
+        System.out.println("This is branching version 6");
         System.out.println("This is branching version 7");
+        System.out.println("This is branching version 8");
 
         // TODO: implement your handler
         return "Hello from Lambda";
