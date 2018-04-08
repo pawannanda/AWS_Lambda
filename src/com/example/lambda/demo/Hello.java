59e9e7cbf2f54abc174962faadde2a9ea62a003e
amitadmin <amitnanda0218@gmail.com>
2018-04-08 21:26:04 +0100

    Learning git

---
+++ src/com/example/lambda/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return "Hello from Lambda";
+    }
